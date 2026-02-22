4bd73bb182deb8eb5517aafd3e354ef5712872eb
Kaizzz <karllastimoso454@gmail.com>
2026-02-22 18:20:26 +0800

    PA 3 Class-Pokemon

--- pokemon_api/lib/Class/pokemon.dart
+++ pokemon_api/lib/Class/pokemon.dart
@@ -1,25 +1,39 @@
 class Pokemon {
   final int id;
   final String name;
-  final String imageUrl;
+  final Sprites sprites;
   final int height;
   final int weight;
 
   Pokemon({
     required this.id,
     required this.name,
-    required this.imageUrl,
+    required this.sprites,
     required this.height,
     required this.weight,
   });
 
   factory Pokemon.fromJson(Map<String, dynamic> json) {
     return Pokemon(
-      id: json['id'],
-      name: json['name'],
-      imageUrl: json['sprites']['front_default'],
-      height: json['height'],
-      weight: json['weight'],
+      id: json['id'] ?? 0,
+      name: json['name'] ?? '',
+      sprites: Sprites.fromJson(json['sprites'] ?? {}),
+      height: json['height'] ?? 0,
+      weight: json['weight'] ?? 0,
+    );
+  }
+}
+
+class Sprites {
+  final String frontDefault;
+
+  Sprites({
+    required this.frontDefault,
+  });
+
+  factory Sprites.fromJson(Map<String, dynamic> json) {
+    return Sprites(
+      frontDefault: json['front_default'] ?? '',
     );
   }
 }
